f902bd11f3c37a47fbe78cf3b9306f5e5dbccc15
drakshi <hagimarushishibala@yahoo.com>
2021-07-07 14:18:28 +0530

    Portfolio Website

---
+++ header.php
@@ -0,0 +1,88 @@
+<link href="css/style3.css" rel="stylesheet" type="text/css" >
+
+  <div class="language-wrapper-top"></div><div class="language-wrapper-bottom"></div></div><script src="https://d1tdp7z6w94jbb.cloudfront.net/js/jquery-3.3.1.min.js" type="text/javascript" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script><script src="https://uploads-ssl.webflow.com/5945c0691ffdf51725f42d2f/js/blaiseposmyouck.6aad8e7d5.js" type="text/javascript"></script>
+<style>
+    .navlink{
+        white-space: nowrap;
+        margin-right: -10px;
+        padding: 40px;
+    }
+    .w-nav-link{
+        margin-left: -10px;
+    }
+    html,body,div,span,applet,object,iframe,h1,h2,h3,h4,h5,h6,p,blockquote,pre,a,img,abbr,acronym,address,big,cite,code,del,dfn,em,font,ins,kbd,q,s,samp,small,strike,strong,sub,sup,tt,var,dl,dt,dd,ol,ul,li,fieldset,form,label,legend,table,caption,tbody,tfoot,thead,tr,th,td,article,aside,canvas,details,figcaption,figure,hgroup,menu,nav,summary,time,mark,audio,video,header,footer {
+  margin:0;
+  padding:0;
+  border:0;
+  font-weight:inherit;
+  font-style:inherit;
+  font-size:100%;
+  vertical-align:baseline;
+  background:transparent;
+  font-family: 'europa',sans-serif!important;
+
+ }
+
+</style>
+    <section id="homeBanner" style="font-family:sans-serif;!important">
+<div data-w-id="08d7d27d-16a8-b8a8-d169-a6e35e7c49a0" class="new-body">
+            <div data-collapse="medium" data-animation="default" data-duration="400" class="navbar w-nav">
+                <div class="navbar-container">
+                    <a data-ix="navlink-transition-wrapperwhite" href="index.php" class="logo-link w-nav-brand"><div class="logo-text">D.</div></a>
+                    <nav role="navigation" class="navmenu w-nav-menu"><a href="projects.php" data-ix="navlink-transition-wrapperwhite" class="navlink w-nav-link w--current"  style=" white-space: 'nowrap';">Projects</a><a href="skills.php" data-ix="navlink-transition-wrapperwhite" class="navlink w-nav-link">Skills And Interests</a></nav><div class="w-nav-button"><div class="w-icon-nav-menu"></div></div>
+                    
+                    
+                </div>
+                </div>
+            </div>
+            <div class="navbar-mobile">
+                <div class="navbar-mobile-container">
+                    <div class="logo-link-mobile w-nav-brand w--current">
+                    </div>
+                    <div class="navmenu-mobile navmenu-mobile-white">
+                        <div data-ix="initial-block" class="navmenu-mobile-wrapper navmenu-mobile-wrapper-white">
+                        </div>
+                        
+                        <div class="navmenu-mobile-container"><a href="projects.php" data-ix="navlink-transition-wrapperwhite" class="navlink-mobile navlink-mobile-blue w-inline-block w--current"><div>Projects</div></a><a href="skills.php" data-ix="navlink-transition-wrapperwhite" class="navlink-mobile navlink-mobile-blue w-inline-block"><div>Skills and Interests</div></a></div>
+                        
+                        
+    <div class="navmenu-mobile-logo-div navmenu-language-div-mobile navmenu-language-div-mobile-blue"><div class="language-text-mobile"><a href="index.php" class="language-link-mobile language-en language-en-blue"  data-ix="navlink-transition-wrapperwhite" class="language-link-mobile language-fr language-fr-blue" style="font-size:40px">D.<span class="span-language-portrait"></span></a>  <a href="index.php" class="language-link-mobile language-en language-en-blue" class="language-link-mobile language-fr language-fr-blue" style="font-size:40px"><span class="span-language-portrait"></span></a></div></div>
+    
+    </div>
+    
+                    
+                    <a href="#" data-ix="menumobile" class="menu-button-mobile w-inline-block">
+                        <div class="menu-button-mobile-div">
+                            <div class="menu-mobile-line-top">
+                                <div class="menu-line-top-wrapper">
+                                </div>
+                            </div>
+                            <div class="menu-mobile-line-mid">
+                            </div>
+                            <div class="menu-mobile-line-bottom">
+                                <div class="menu-line-bottom-wrapper">
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+        </div>
+        
+<script>
+$('a.navlink, a.portfolio-next-fixed-link, a.portfolio2-image-link, a.portfolio-body-left-link, a.logo-link, a.navlink-mobile, a.footer-link-project-mobile, a.portfolio-mobile-link, a.language-link, a.language-link-mobile').click(function (e) {
+    e.preventDefault();                   // prevent default anchor behavior
+    var goTo = this.getAttribute("href"); // store anchor href
+
+    setTimeout(function(){
+         window.location = goTo;
+    },1900);       
+});
+</script>
+<script>
+  $(window).bind("pageshow", function(event) {
+    if (event.originalEvent.persisted) {
+        window.location.reload() 
+    }
+});
+</script>
+</section>
---
+++ projects.php
@@ -0,0 +1,287 @@
+
+
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <title>PROJECTS | DRAKSHI CHOPRA</title>
+  <meta name="viewport" content="width=device-width, initial-scale = 1.0, maximum-scale=1.0, user-scalable=no" />
+  <link href='http://fonts.googleapis.com/css?family=PT+Sans' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Droid+Serif:regular,bold" />
+
+  <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Alegreya+Sans:regular,italic,bold,bolditalic" />
+  <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Nixie+One:regular,italic,bold,bolditalic" />
+  <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Alegreya+SC:regular,italic,bold,bolditalic" />
+  <link rel="stylesheet" href="style.css" type="text/css" media="screen" />
+  <link rel="stylesheet" href="css/main.css">  
+  <link rel="stylesheet" href="css/hover.css"> 
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>  
+  <link rel='stylesheet' id='slideshow-css'  href='css/flexslider4b1d.css?ver=1.8' type='text/css' media='all' />
+  <script type='text/javascript' src='js/jqueryb8ff.js?ver=1.12.4'></script>
+  <script type='text/javascript' src='js/jquery-migrate.min330a.js?ver=1.4.1'></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js" type="text/javascript"></script>
+  
+  <style type="text/css" media="screen">
+      
+
+      ul#tabs-nav{
+          list-style: none; 
+          margin: 0;
+          padding: 5px;
+          overflow: auto;
+          display: flex;
+          justify-content: center;
+      }  
+    
+  #bannerText .main {/*  margin-top: -15px; */ }
+  blockquote, address {
+    border-left: 5px solid #85bec4;
+  } 
+  #content .project.small .inside{
+    background-color: #85bec4 !important;
+  } 
+  #pageslide {
+    background-color: #333333;
+  } 
+  #header .top.solid, #header .top, .has-banner #header .top.solid { background-color: rgba(0,0,0,0);}
+  a { color: #4da7ca;}
+  a:hover {color: #4290ae;}
+  .button, #searchsubmit, input[type="submit"] {background-color: #52a3ba;}
+
+  #homeBanner {
+    background: url(img/dock.jpg) no-repeat center center fixed;   
+    -webkit-background-size: 100%; 
+    -moz-background-size: 100%; 
+    -o-background-size: 100%; 
+    background-size: 100%;
+    background-size: cover;
+    -webkit-background-size: cover; 
+    -moz-background-size: cover; 
+    -o-background-size: cover;      
+    overflow: hidden;
+    width: 100%;    
+    margin: 0;  
+    position: relative;
+  }
+
+  .menu-item-326{
+    color: rgba(255,255,255,1) !important;
+    border: 1px solid #bc9a5c;
+    display: block;
+    background: #bc9a5c;
+    padding: 10px 13px;
+  }
+  .navbar-container{
+    border-right:none !important;
+  }
+  a:hover{
+    text-decoration:none;
+  }
+  .sub-head{
+    font-weight: 200;
+    color: rgba(255,255,255,0.7);
+    position: relative;
+    top: -2px;
+    letter-spacing: 6px;
+    margin-top:20px;
+    margin-bottom:20px;
+    
+  }
+  .sub-head:hover{
+    text-decoration:none;
+    color:rgba(255,255,255,0.8);
+    transition: 1s;
+  }
+  .image {
+    position: relative;
+    width: 200px !important;
+}
+.image2{
+  margin-top:20px;
+  margin-bottom:20px;
+  position: relative;
+}
+.image__img {
+    display: block;
+    width: 100%;
+    filter: grayscale(0.6) brightness(0.8);
+}
+
+.image__overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.6);
+    color: #ffffff;
+    
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    opacity: 0;
+    transition: opacity 0.25s;
+}
+
+.image__overlay--blur {
+    backdrop-filter: blur(5px);
+}
+
+.image__overlay--primary {
+    background: rgba(0,0,0,0.7);
+}
+
+.image__overlay > * {
+    transform: translateY(20px);
+    transition: transform 0.25s;
+}
+
+.image__overlay:hover {
+    opacity: 1;
+}
+
+.image__overlay:hover > * {
+    transform: translateY(0);
+}
+
+.image__title {
+    font-size: 2em;
+    
+}
+
+.title{
+  margin-top: 10px;
+    margin-bottom: 10px;
+    color: rgba(255,255,255);
+}
+.read-btn{
+    border: 1px solid #fff;
+    padding: 10px ;
+    color : #fff;
+    cursor: pointer;
+  }
+  .read-btn:hover{
+    background: rgba(255,255,255,0.9) !important;
+    color: #121212 !important;
+    transition: 1s ;
+  }
+</style>
+
+</head>
+
+<body>
+
+  <!-- Navigation Bar starts-->
+  <?php include "./header.php"; ?>
+  <!-- Navigation Bar ends-->
+
+    <section>
+ <div id="contaner">  
+  <div id="header">
+      <div class="page-content" id="homeBanner">
+      <div class="honors-block">
+               <h1 class="text-center" style="text-align: center; font-size:40px;">
+                      <span data-ix="homespan1">PROJECTS</span>
+
+                  </h1>
+                  
+          <div style="padding-top:100px;">
+          <p class="text-left">I have designed and buit websites and projects for companies and startups. Here's a small collection of my latest works.</p>
+         <h3 class="text-left sub-head" data-ix="homespan1">CHECK OUT MY RECENT WORKS </h3>
+        <div class="honor-ind" data-ix="homeparag">
+        <div class="container">
+  <div class="row">
+    <div class="col-lg">
+    <a href="http://www.theerbeproject.com/about.php" target="_blank">
+    <div class="image">
+    <img class="image__img" src="img/erbe.png" alt="Bricks">
+    <div class="image__overlay image__overlay--primary">
+        <div class="image__title">View</div>
+    </div>
+    <div class="title">THE ERBE PROJECT</div>
+</div> 
+</a>
+    </div>
+    <div class="col-lg">
+    <a href="http://theornatechronicle.com/" target="_blank">
+    <div class="image">
+    <img class="image__img" src="img/TOC.png">
+    <div class="image__overlay image__overlay--primary">
+        <div class="image__title">View</div>
+    </div>
+    <div class="title">THE ORNATE CHRONICLE</div>
+</div> 
+</a>
+    </div>
+    <div class="col-lg">
+    <a href="http://www.histare.net/" target="_blank">
+    <div class="image">
+    <img class="image__img" src="img/Histare.png">
+    <div class="image__overlay image__overlay--primary">
+        <div class="image__title">View</div>
+    </div>
+    <div class="title">HISTARE</div>
+</div> 
+</a>
+    </div>
+  </div>
+</div>
+<h3 class="text-left sub-head" data-ix="homespan1">SOME PERSONAL PROJECTS</h3> 
+<a href="https://drakshi.github.io/CodePlayer-jQuery/" target="_blank">
+    <div class="image2">
+    <img class="image__img" src="img/codePlayer.png">
+    <div class="image__overlay image__overlay--primary">
+        <div class="image__title">View</div>
+    </div>
+    <div class="title">CODE PLAYER</div>
+    <div class="title">Get a live representation of your code</div>
+</div> 
+</a>
+
+<a href="https://drakshi.github.io/ExpenseTrackerReact/" target="_blank">
+    <div class="image2">
+    <img class="image__img" src="img/Expense.png">
+    <div class="image__overlay image__overlay--primary">
+        <div class="image__title">View</div>
+    </div>
+    <div class="title">EXPENSE TRACKER</div>
+    <div class="title">Helps you to keep track of your expenses.</div>
+</div> 
+</a><br><br>
+<a class="read-btn" href="https://github.com/drakshi" target="_blank">SEE MORE ON GITHUB</a>
+</div>
+        </div>
+  
+     </div>          
+   </div>
+
+
+
+
+  <!-- END tabs-nav -->
+  <div data-ix="wrapper-white" class="a-propos-wrapper"></div>
+                <div class="wrapper-white"></div>
+                <div class="apropos-bg-container">
+                 
+                </div>
+    </section>
+
+
+<script type='text/javascript' src='js/comment-reply.min55fe.js?ver=4.9.5'></script>
+<script type='text/javascript' src='js/jquery.fitvids5152.js?ver=1.0'></script>
+<script type='text/javascript' src='js/jquery.actual.min7b5a.js?ver=1.8.13'></script>
+<script type='text/javascript' src='js/jquery.waitforimages.min4c56.js?ver=2.0.2'></script>
+<script type='text/javascript' src='js/jquery.pageslide.mind5f7.js?ver=2.0'></script>
+<script type='text/javascript' src='js/jquery.scrollTo.min5f04.js?ver=1.4.6'></script>
+<script type='text/javascript' src='js/jquery.isotope.mindc81.js?ver=1.5.25'></script>
+<script type='text/javascript' src='js/jquery.flexslider-min4b1d.js?ver=1.8'></script>
+<script type='text/javascript' src='js/theme_trust5152.js?ver=1.0'></script>
+<script type='text/javascript' src='js/wp-embed.min55fe.js?ver=4.9.5'></script>
+</body>
+
+</html>
